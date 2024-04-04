6fe542e065975f1aecea487e521ee6d7c5b25a67
yunjaehun <wogns8030@naver.com>
2024-04-04 23:28:03 +0900

    init js

---
+++ assets/scripts/app.js
@@ -0,0 +1,9 @@
+const firstStart = 0;
+
+currentResult = firstStart + 10 * 3;
+
+let calculationDescription = `(${firstStart} + 10) * 3`;
+
+outputResult(currentResult, calculationDescription);
+
+let system = `1 + ${1 + 1}`
