e3172728cc84e890d0e505c3d4202fc177036fbd
yunjaehun <wogns8030@naver.com>
2024-04-04 23:50:24 +0900

    function 함수 사용

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,7 +3,11 @@ const firstStart = 0;
 currentResult = firstStart + 10 * 3;
 
 let calculationDescription = `(${firstStart} + 10) * 3`;
-
 outputResult(currentResult, calculationDescription);
 
-let system = `1 + ${1 + 1}`
+function add(num1, num2) {
+  const result = num1 + num2;
+  alert('THe Result is : ' + result);
+}
+
+add(1,2)
